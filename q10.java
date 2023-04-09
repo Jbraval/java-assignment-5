9ea39e363b52f6b0dab2e170b297b5b4bbf34207
Jbraval <128069825+Jbraval@users.noreply.github.com>
2023-04-09 18:11:26 +0530

    Add files via upload

---
+++ q10.java
@@ -0,0 +1,8 @@
+import java.util.Scanner; 
+public class q10 { 
+ public static void main(String[] args) { 
+ Scanner scn = new Scanner(System.in); 
+ int n = scn.nextInt(); 
+ for(int i = 1; i * i <= n; i++){ 
+ System.out.print(i*i + " "); 
+ }}}
---
+++ q2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+  
+public class q2 { 
+ public static void main(String[] args) { 
+ Scanner scn = new Scanner(System.in); 
+ int sum = 0; 
+ while(scn.hasNextInt())
+ { 
+ int num = scn.nextInt(); 
+ if(num >= 0){ 
+ } 
+ else{ 
+ sum = -1;
+ } 
+ } 
+ System.out.print(sum); 
+ } 
+}
